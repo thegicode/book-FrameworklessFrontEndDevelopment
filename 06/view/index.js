c4f679908df5697a860ee06465f5dd0d7546c01c
thegicode <thegi.code@gmail.com>
2021-04-07 22:00:50 +0900

    Add 06

---
+++ 06/view/index.js
@@ -0,0 +1,13 @@
+import createRouter from './router.js'
+import createPages from './pages.js'
+
+const container = document.querySelector('main')
+
+const pages = createPages(container)
+const router = createRouter()
+
+router
+    .addRoute('#/', pages.home)
+    .addRoute('#/list', pages.list)
+    .setNotFound(pages.setNotFound)
+    .start();
---
+++ 06/view/pages.js
@@ -0,0 +1,21 @@
+export default container => {
+    const home = () => {
+        container
+            .textContent = 'This is Home page'
+    }
+
+    const list = () => {
+        container
+            .textContent = 'This is list page'
+    }
+
+    const notFound = () => {
+        container
+            .textContent = 'Page Not Found'
+    }
+
+    return {
+        home,
+        list
+    }
+}
